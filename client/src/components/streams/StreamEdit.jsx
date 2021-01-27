6351499cbd5ee4e3da2db2dfb0a6d9b0fabfce34
denys_gl <oggyla@gmail.com>
2021-01-26 18:54:08 -0800

    intial commit #2 :D

---
+++ client/src/components/streams/StreamEdit.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StreamEdit = () => {
+  return (
+    <>
+    <h1>StreamEdit</h1>
+    </>
+  )
+};
+
+export default StreamEdit;
---
+++ client/src/components/streams/StreamShow.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StreamShow = () => {
+  return (
+    <>
+    <h1>StreamShow</h1>
+    </>
+  )
+};
+
+export default StreamShow;
