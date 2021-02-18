464ddb98ab93ab6c64e2aa78e583a5d1b13303c3
DESKTOP-VRPGC5L\hitman51 <denis.glukhovskyy@gmail.com>
2021-02-18 09:06:48 -0800

    added redux-thunk to the project as a middleware that handles async actions

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
-
+import reduxThunk from 'redux-thunk'; //our middleware to handle async actions
 
 import App from './components/App';
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk))); //added reduxThunk to applyMiddleware 
 
-ReactDOM.render( <Provider store = { store } > <App /> </Provider> ,
+ReactDOM.render( < Provider store = { store } > < App / > < /Provider> ,
     document.querySelector('#root')
 );
