3c76871e16a2beb5ae0a0031751c062f777a4f0f
denys_glu <denis.glukhovskyy@gmail.com>
2021-02-19 17:51:58 -0800

    Refactoring StreamCreate; adding StreamForm component;

--- client/src/components/streams/StreamEdit.jsx
+++ client/src/components/streams/StreamEdit.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions';
+ 
+const StreamEdit = (props) => {
+  useEffect(()=> {
+    props.fetchStream(props.match.params.id);
+  }, []);
 
-const StreamEdit = () => {
   return (
     <>
-    <h1>StreamEdit</h1>
+    {
+      props.stream ? <div>{props.stream.title}</div>
+                   : <div>Loading...</div>
+    }
     </>
   )
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] }
+}
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
